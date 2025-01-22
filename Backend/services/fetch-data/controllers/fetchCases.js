f358dba71a775eecff4a611cbdd8d1732ef2ad80
WaritTeerapong <smart29952@gmail.com>
2025-01-22 23:24:57 +0700

    Update variable name in fetchCases
    updated_cases -> enriched_cases

--- Backend/services/fetch-data/controllers/fetchCases.js
+++ Backend/services/fetch-data/controllers/fetchCases.js
@@ -34,7 +34,7 @@ const fetchCases = async (req, res) => {
       });
     }
 
-    const updated_cases = await Promise.all(
+    const enriched_cases = await Promise.all(
       cases.map(async (caseItem) => {
         // query image
         const image = await getImageCount(caseItem.AN);
@@ -69,7 +69,7 @@ const fetchCases = async (req, res) => {
     // Fetch case succesfully
     return res.status(200).json({
       message: RESPONSE_MESSAGES.taskSuccess,
-      data: updated_cases,
+      data: enriched_cases,
     });
   } catch (error) {
     console.error("Error in fetch case controller ->", error.message);
