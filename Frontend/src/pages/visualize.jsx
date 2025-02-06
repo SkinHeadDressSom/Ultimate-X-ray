c8152e341353419d5948bb656896c939e154f400
yabaina <107362148+yabaina@users.noreply.github.com>
2025-02-06 15:09:17 +0700

    จานสีส้นตีนบัคเชี่ยไรเยอะแยะแม่งูเอ้ย

--- Frontend/src/pages/visualize.jsx
+++ Frontend/src/pages/visualize.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ContrastPopup from '../components/tools/contrastpop'
-import Colorpopup from '../components/tools/colorpop'
+
 
 const Visualize = () => {
   return (
-    <div> <ContrastPopup/> </div>
+    <div> <ContrastPopup /> </div>
   )
 }
 
