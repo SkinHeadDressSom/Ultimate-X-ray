3f8b136ba6f671c0da36309aef79cdb13ac010c3
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2025-02-13 16:59:44 +0700

    popup position

--- Frontend/src/components/Visualize/Sidebar/tools/ImageTools.jsx
+++ Frontend/src/components/Visualize/Sidebar/tools/ImageTools.jsx
@@ -29,6 +29,7 @@ const ImageTools = ({
   const [activeId, setActiveId] = useState("pointer"); // ค่าเริ่มต้นเป็น pointer
   const [showContrastPopup, setShowContrastPopup] = useState(false);
   const [showColorPopup, setShowColorPopup] = useState(false);
+  const [popupPosition, setPopupPosition] = useState("550px");
 
   const buttons = [
     { id: "undo", icon: Undo },
@@ -60,6 +61,7 @@ const ImageTools = ({
         setIsDragMode((prev) => !prev);
       }
       if (id === "highlight") {
+        setPopupPosition("500px");
         setIsDrawMode((prev) => !prev);
         setSelectedShape(id);
         setShowColorPopup(true);
